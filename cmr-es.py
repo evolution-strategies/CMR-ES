682438163b4282768d0879b40bcfd18dd1298fe9
evolution-strategies <109295043+evolution-strategies@users.noreply.github.com>
2022-09-26 18:57:27 +0200

    Update cmr-es.py

--- cmr-es.py
+++ cmr-es.py
@@ -1,70 +1,79 @@
-# CMR-ES presented on ESANN 2022 
-# Paper "Evolution Strategies with Rechenberg Mutation Rate Control and Covariance Matrix Estimation" 
+# CMR-ES proposed on EASNN 2022
+# Paper "A Fast and Simple Evolution Strategy with Covariance Matrix Estimation"
 # by Oliver Kramer, Computational Intelligence Lab, University of Oldenburg
 
-
 import numpy as np
+import math
 
 N = 5
-scale = 1000.
+kappa = 200
+eta = 20
 
-def scalesphere(x):
-	# first dimension scaled by "scale"
-	w=np.ones(N)
-	w[0]=scale
-	x=w*x
-	return np.dot(x,x)
+archive = []
+
+def rosenbrock(x):
+	return np.sum([100*(x[i+1]-x[i]**2)**2 + (1-x[i])**2 for i in range(N-1)]) 
 
 
-fitfunction = scalesphere
+def fitfunction(x):
+	return rosenbrock(x)
 
 
 def termination(t,fit):
-	return True if (t>10000 or fit < 10e-500) else False
+	return True if (t>3000 or fit < 10e-500) else False
+
+
+def estimate_C(x_, sigma):
+	global archive
+	X_ = np.array(archive)
+	C = np.cov(X_.T)
+	return C
 
 
 def CMRES():
-	
-	x = np.ones(N)		# objective variables initialized at (1,1,..)
-	fit = fitfunction(x)
-	sigma = 1.			# step size 
-	t = 0
 
+	global archive
 	fitlog = []
+	t=1
 	happy = False
+	sigma = 0.5	
 
-	print("Another CMR-ES run...")
+	x = np.zeros(N)
+	fit = fitfunction(x)
+	C = np.identity(N)
+	d = math.sqrt(N)
 
-	# covariance estimation
-	X = np.array([np.random.randn(N) for i in range(2000)])
-	X =np.array([x[1] for x in sorted([(fitfunction(x),x) for x in X], key=lambda x: x[0])][:20])
-	C = np.cov(X.T)		# this function does the work
-	print("C",C)
-
-
-	# (1+1)-ES 
 	while not happy:
 
-		t+=1
 		happy = termination(t,fit)
 		x_ = x + sigma * np.random.multivariate_normal(np.zeros(N),C)
 		fit_ = fitfunction(x_)
 
-		# Rechenberg 1/5 success rule and selection
 		if fit_ <= fit:
 			x = x_
 			fit = fit_
-			sigma*=np.exp(4/5)
+			sigma*=np.exp(4/5/d)
+			archive.insert(0,x)
+			archive=archive[:eta]
+
 		else: 
-			sigma*=np.exp(-1/5)
+			sigma*=np.exp(-1/5/d)
 
 		fitlog.append(fit)
+
+		if t%kappa==0:
+			C = estimate_C(x_=x,sigma=sigma)
+
+		t+=1
+
+		print("Fitness in generation",t,"is",fit)
 
 	return fitlog
 
 
 runs = [CMRES() for i in range(10)]
 
+print("25 CMR-ES runs on Rosenbrock, N=",N)
 
 print("mean",np.mean([runs_[-1] for runs_ in runs]))
 print("std",np.std([runs_[-1] for runs_ in runs]))
